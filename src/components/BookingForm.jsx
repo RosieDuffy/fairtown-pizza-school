27e709a391afffe2a4a5d0e499987b78f45409c8
Rosie Duffy <rose.j.duffy@gmail.com>
2024-03-08 18:56:27 +0000

    test

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -16,7 +16,7 @@ const BookingForm = () => {
   return (
     <>
       <div className="booking-container">
-        <Link to="/">
+        <Link to="//">
           <button className="exit-button">&times;</button>
         </Link>
         <div className="booking-form">
