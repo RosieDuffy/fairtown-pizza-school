467996d1a8374e9043526570ce70b18fcb563ee7
Rosie Duffy <rose.j.duffy@gmail.com>
2024-03-08 17:37:53 +0000

    images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <>
       <div className="contact-container" id="contact">
         <img
-          src="./assets/location.webp"
+          src="public/assets/location.webp"
           alt="fairtown pizza school location"
         />
         <div className="contact">
